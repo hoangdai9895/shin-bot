d0cb71e57de382758ef5fd566d792822f5653b63
dailch <hoangdai9895@gmail.com>
2020-08-05 02:03:10 +0700

    mention test

--- index.js
+++ index.js
@@ -188,9 +188,7 @@ const reminder = (xxx) => {
           conversationReference,
           async (turnContext) => {
             const boardId = "5f14457b33a4275b58d553a4";
-            let contextText = `Các task trong ngày:\n\n\u200C @${
-              turnContext.activity.recipient.name
-            } ${JSON.stringify(turnContext.activity.from)}`;
+            let contextText = `Các task trong ngày:\n\n\u200C @Đại ${JSON.stringify(turnContext.activity.from)}`;
             let list = {};
             let members = [];
             let cards = [];
